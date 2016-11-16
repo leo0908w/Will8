0e93a20bb36bfa5e1daf0a8758be1630a5bfaf63
Will <leo0908w@yahoo.com.tw>
2016-11-16 12:32:37 +0800

    v1

--- app/src/main/java/com/org/iii/will08/GameView.java
+++ app/src/main/java/com/org/iii/will08/GameView.java
@@ -6,9 +6,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameView extends View {
     private Resources res;
@@ -16,6 +20,9 @@ public class GameView extends View {
     private int viewW, viewH;
     private boolean isInit;
     private Bitmap bmpBall;
+    private Matrix matrix;
+    private float ballW, ballH, ballX, ballY, dx, dy;
+    Timer timer;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -23,14 +30,45 @@ public class GameView extends View {
 //        setBackgroundColor(Color.YELLOW);
         this.context = context;
         res = context.getResources();
+        matrix = new Matrix();
+        timer = new Timer();
     }
 
     private void init() {
         viewH = getHeight();
         viewW = getWidth();
         Log.v("will", viewW + ",, " + viewH);
+
+        dx = 10;    dy = 10;
         bmpBall = BitmapFactory.decodeResource(res, R.drawable.b0);
+        ballW = viewW / 12f;    ballH = ballW;
+        bmpBall = resizeBmp(bmpBall, ballW, ballH);
+
+        timer.schedule(new BallTask(), 1000, 60);
+
         isInit = true;
+    }
+    
+
+    private class BallTask extends TimerTask {
+        @Override
+        public void run() {
+            if (ballX < 0 || ballX + ballW > viewW) {
+                dx *= -1;
+            }
+            if (ballY < 0 || ballY + ballH > viewH) {
+                dy *= -1;
+            }
+            ballX += dx;    ballY += dy;
+            postInvalidate();
+        }
+    }
+
+    Bitmap resizeBmp(Bitmap src, float w, float h) {
+        matrix.reset();
+        matrix.postScale(ballW / bmpBall.getWidth(), ballH / bmpBall.getHeight());
+        return Bitmap.createBitmap(
+                bmpBall, 0, 0, bmpBall.getWidth(), bmpBall.getHeight(), matrix, false);
     }
 
     @Override
@@ -38,6 +76,6 @@ public class GameView extends View {
         super.onDraw(canvas);
 
         if (!isInit) init();
-        canvas.drawBitmap(bmpBall, 0, 0, null);
+        canvas.drawBitmap(bmpBall, ballX, ballY, null);
     }
 }
--- app/src/main/java/com/org/iii/will08/MainActivity.java
+++ app/src/main/java/com/org/iii/will08/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private BradView bradView;
+//    private BradView bradView;
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(bradView);
 
         setContentView(R.layout.activity_main);
+        gameView = (GameView) findViewById(R.id.gameView);
+    }
+
+    @Override
+    public void finish() {
+        if (gameView.timer != null) {
+            gameView.timer.purge();
+            gameView.timer.cancel();
+            gameView.timer = null;
+        }
+        super.finish();
     }
 }
